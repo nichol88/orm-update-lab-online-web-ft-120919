56c7dafb4d826a4e36cad4b6ae35fd366351216b
Brian Nicholls <nichol88@gmail.com>
2020-01-22 18:09:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,9 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   def initialize(name, grade, id = nil)
-
+    @id = id
+    @name = name
+    @grade = grade
   end
 
 end
