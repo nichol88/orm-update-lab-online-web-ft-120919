cea1e293a2f13ce92ca9a94034f2d089df382cd3
Brian Nicholls <nichol88@gmail.com>
2020-01-22 18:09:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,8 @@ class Student
   attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  def initialize(name, grade, id = nil)
 
+  end
 
 end
