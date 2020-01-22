0b2d502aa5ff2801354a57365ccee3b472e167ba
Brian Nicholls <nichol88@gmail.com>
2020-01-22 18:37:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,11 +51,6 @@ class Student
     s.name = row[1]
     s.grade = row[2]
     s
-    sql = <<-SQL
-
-    SQL
-
-    DB[:conn].execute(sql, name).map{ |row| self.new_from_db(row)}
   end
 
   def self.find_by_name(name)
